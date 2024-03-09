3aa86480a3cfbca547dafb8c0ffaab51bd865ff4
Wang Zhipeng <13476859295@163.com>
2024-03-09 23:30:35 +0800

    Update model.py

--- src/models/API/chatgpt/model.py
+++ src/models/API/chatgpt/model.py
@@ -1,4 +1,41 @@
 
 class Model():
     def __init__(self, modelname, selected_lora_model, selected_gpu):
-        self.model
+        pass
+    def generate(self, inputs, original_language, target_languages, max_batch_size):
+        """
+            return sample:
+            [
+                [
+                    {
+                        "target_language":"English",
+                        "generated_translation":"I love you",
+                    },
+                    {
+                        "target_language":"Chinese",
+                        "generated_translation":"我爱你",
+                    },
+                ],
+                [
+                    {
+                        "target_language":"English",
+                        "generated_translation":"Who's your daddy",
+                    },
+                    {
+                        "target_language":"Chinese",
+                        "generated_translation":"谁是你爸爸",
+                    },
+                ],
+                [
+                    {
+                        "target_language":"English",
+                        "generated_translation":"Today is Friday",
+                    },
+                    {
+                        "target_language":"Chinese",
+                        "generated_translation":"今天是星期五",
+                    },
+                ],
+            ]
+        """
+        pass
